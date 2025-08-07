3b4b65e3c174ebcdf968cbb0fefa0e71988a1624
gabicantero <contatogabicantero@gmail.com>
2025-08-07 12:45:32 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Rat Cage Manager", layout="wide")
+
+st.title("🐭 Rat Cage Manager")
+st.write("Welcome! This is your cage visualization and management system.")
