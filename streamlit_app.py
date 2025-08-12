b288e3fd17e7ed4abf6bac55f57db5cb02e841cf
gabicantero <contatogabicantero@gmail.com>
2025-08-12 18:58:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import datetime
+import os 
 
 st.set_page_config(page_title="Rat Cage Manager", layout="wide")
 st.title("Animal Cage Manager")
@@ -15,18 +16,15 @@ def load_data():
                 "Real DOB Puppies", "Weaning Date", "Milking Days Done"]
         return pd.DataFrame(columns=cols)
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-PROJECTS_PATH = os.path.join(BASE_DIR, "projects.csv")
-
 def load_projects():
-    if os.path.exists(PROJECTS_PATH):
-        return pd.read_csv(PROJECTS_PATH)
-    else:
-        columns = ["Project", "Description"]
-        return pd.DataFrame(columns=columns)
+    try:
+        df = pd.read_csv("projects.csv")
+        if df.empty:
+            df = pd.DataFrame(columns=["Project", "Description", "Experiements"])
+        return df
+    except FileNotFoundError:
+        return pd.DataFrame(columns=["Project", "Description","])
 
-def save_projects(df):
-    df.to_csv(PROJECTS_PATH, index=False)
 
 # Carrega os dados no início da página
 
