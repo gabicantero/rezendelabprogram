fd95ef2b3ed0377110237ea571828a51518df9d0
gabicantero <contatogabicantero@gmail.com>
2025-08-13 16:11:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -285,7 +285,6 @@ if page == "Projects":
     with st.form("add_project_form"):
         new_proj_name = st.text_input("Project Name")
         new_proj_desc = st.text_area("Project Description")
-        n_exp = st.number_input("Number of Experiments", min_value=1, max_value=10, value=3, step=1)
 
         new_exp_names = []
         new_exp_dates = []
